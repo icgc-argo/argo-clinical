1ee836832ae5d0a063ad780f4b6098ad356697b6
Robert Tisma <rtisma@gmail.com>
2019-06-10 22:12:52 -0400

    added donor table

--- src/main/resources/flyway/sql/V1__initial_database.sql
+++ src/main/resources/flyway/sql/V1__initial_database.sql
@@ -1,4 +1,9 @@
 CREATE TABLE donor
 (
-    id                UUID PRIMARY KEY
+    id                UUID UNIQUE NOT NULL,
+    submitter_id      VARCHAR(255) NOT NULL,
+    program_id        UUID NOT NULL,
+    entity_id         SERIAL NOT NULL UNIQUE ,
+    PRIMARY KEY (submitter_id, program_id)
 );
+
