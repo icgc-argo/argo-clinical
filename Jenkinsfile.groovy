2ff7c05533a3077795cdaae5ec78dfb8e6e94d72
ballabadi <bashar.allabadi@oicr.on.ca>
2019-07-30 14:39:57 -0400

    fix channel var name

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -111,7 +111,7 @@ spec:
         container("node") {
           script {
             if (env.BRANCH_NAME == "master" || env.BRANCH_NAME == "develop") {
-              withCredentials([string(credentialsId: 'JenkinsFailuresSlackChannelURL', variable: 'SLACK_URL')]) { 
+              withCredentials([string(credentialsId: 'JenkinsFailuresSlackChannelURL', variable: 'JenkinsFailuresSlackChannelURL')]) { 
                 sh "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"Build Failed: ${env.JOB_NAME} [${env.BUILD_NUMBER}] (${env.BUILD_URL}) \"}' ${JenkinsFailuresSlackChannelURL}"
               }
             }
@@ -123,7 +123,7 @@ spec:
         container("node") {
           script {
             if (env.BRANCH_NAME == "master" || env.BRANCH_NAME == "develop") {
-              withCredentials([string(credentialsId: 'JenkinsFailuresSlackChannelURL', variable: 'SLACK_URL')]) { 
+              withCredentials([string(credentialsId: 'JenkinsFailuresSlackChannelURL', variable: 'JenkinsFailuresSlackChannelURL')]) { 
                 sh "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"Build Fixed: ${env.JOB_NAME} [${env.BUILD_NUMBER}] (${env.BUILD_URL}) \"}' ${JenkinsFailuresSlackChannelURL}"
               }
             }
