175fc7c9541f40d7bbc2a550923598431d039cb7
Jon Eubank <joneubank@gmail.com>
2019-09-26 16:45:40 -0400

    :bug: Delete active submission by programId (#162)

--- src/submission/submission-repo.ts
+++ src/submission/submission-repo.ts
@@ -9,6 +9,7 @@ const L = loggerFor(__filename);
 
 export interface ClinicalSubmissionRepository {
   delete(id: string): Promise<void>;
+  deleteByProgramId(id: string): Promise<void>;
   create(
     command: DeepReadonly<ActiveClinicalSubmission>,
   ): Promise<DeepReadonly<ActiveClinicalSubmission>>;
@@ -61,6 +62,18 @@ export const submissionRepository: ClinicalSubmissionRepository = {
       return;
     } catch (err) {
       throw new InternalError(`failed to delete ActiveSubmission with Id: ${id}`, err);
+    }
+  },
+  async deleteByProgramId(programId: string): Promise<void> {
+    L.debug(`in deleteByProgramId for activeSubmission programId: ${programId}`);
+    try {
+      await ActiveSubmissionModel.deleteOne({ programId }).exec();
+      return;
+    } catch (err) {
+      throw new InternalError(
+        `failed to delete ActiveSubmission with programId: ${programId}`,
+        err,
+      );
     }
   },
   async update(command: DeepReadonly<ActiveClinicalSubmission>): Promise<void> {
