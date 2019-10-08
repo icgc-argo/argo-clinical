0b69f38274c91f149731131159bf97c9ca3c7a97
jaserud <udjaser@gmail.com>
2019-10-08 10:06:58 -0400

    rename

--- src/routes/schema.ts
+++ src/routes/schema.ts
@@ -6,7 +6,8 @@ import * as schemaApi from '../submission/schema-api';
 const router = express.Router();
 
 router.get('/', wrapAsync(schemaApi.get));
-router.get('/all', wrapAsync(schemaApi.getAllSchema));
+// returns a list of all sub-schemas except sample_registration
+router.get('/list', wrapAsync(schemaApi.getAllSchema));
 // get template for a given schema
 router.get('/template/:schemaName', wrapAsync(schemaApi.getTemplate));
 // temporary api
