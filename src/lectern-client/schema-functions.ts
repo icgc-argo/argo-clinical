c13da4ab1310d3d91b4d702d59a9a62de5b3ce86
ballabadi <bashar.allabadi@oicr.on.ca>
2019-10-18 18:33:35 -0400

    normalize user code list only for string values

--- src/lectern-client/schema-functions.ts
+++ src/lectern-client/schema-functions.ts
@@ -129,23 +129,24 @@ const convertFromRawStrings = (
       return undefined;
     }
 
+    const valueType = field.valueType;
+    let formattedFieldValue = record[field.name];
+    const rawValue = formattedFieldValue;
+
     // convert field to match corresponding enum from codelist, if possible
-    if (field.restrictions && field.restrictions.codeList) {
+    if (field.restrictions && field.restrictions.codeList && valueType === ValueType.STRING) {
       const formattedField = field.restrictions.codeList.find(
         e => e.toString().toLowerCase() === record[field.name].toString().toLowerCase(),
       );
       if (formattedField) {
-        mutableRecord[field.name] = formattedField;
+        formattedFieldValue = formattedField as string;
       }
-      return undefined;
     }
 
-    const valueType = field.valueType;
-    const rawValue = record[field.name];
     let typedValue: SchemaTypes = record[field.name];
     switch (valueType) {
       case ValueType.STRING:
-        typedValue = record[field.name];
+        typedValue = formattedFieldValue;
         break;
       case ValueType.INTEGER:
         typedValue = Number(rawValue);
@@ -154,7 +155,8 @@ const convertFromRawStrings = (
         typedValue = Number(rawValue);
         break;
       case ValueType.BOOLEAN:
-        typedValue = Boolean(rawValue);
+        // we have to lower case in case of inconsistent letters (boolean requires all small letters).
+        typedValue = Boolean(rawValue.toLowerCase());
         break;
     }
     mutableRecord[field.name] = typedValue;
