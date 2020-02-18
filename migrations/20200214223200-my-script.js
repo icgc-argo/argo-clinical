192716698fa96310f027a71cbf472920210aec15
ballabadi <bashar.allabadi@oicr.on.ca>
2020-02-18 11:39:08 -0500

    update down sample script

--- migrations/20200214223200-my-script.js
+++ migrations/20200214223200-my-script.js
@@ -1,12 +1,17 @@
 module.exports = {
   async up(db, client) {
     await db.collection('newCollection').insertOne({ _id: '1', key: 'test' });
-    await db.collection('newCollection').insertOne({ _id: '1', key: 'test' });
     await db.collection('newCollection').deleteOne({ _id: '1', key: 'test' });
+    await db.collection('newCollection').drop();
     // you can write some tests to verify everything is good and throw error if not.
   },
 
   async down(db, client) {
-    await db.collection('newCollection').deleteOne({ key: 'test' });
+    // this is not needed but just as an example
+    try {
+      await db.collection('newCollection').drop();
+    } catch (err) {
+      console.error(err);
+    }
   },
 };
