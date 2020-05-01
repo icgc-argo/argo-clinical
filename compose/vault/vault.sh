c2620df65df59af01afdc3cf669a09bee09d440b
ballabadi <bashar.allabadi@oicr.on.ca>
2020-05-01 16:20:35 -0400

    fix vault secrets parsing

--- compose/vault/vault.sh
+++ compose/vault/vault.sh
@@ -3,7 +3,8 @@ fun(){
     RET=1
     until [ ${RET} -eq 0 ]; do
         echo trying to set the secret in vault
-        vault kv put secret/clinical/secrets_v1 content='{"CLINICAL_DB_USERNAME": "admin", "CLINICAL_DB_PASSWORD": "password"}'
+        cd /scripts
+        vault kv put secret/clinical/secrets_v1 @secrets.json
         RET=$?
         sleep 1
     done
