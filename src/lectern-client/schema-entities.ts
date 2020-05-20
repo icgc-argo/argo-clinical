e3fd59e920b63710ddaec7f059dd0a50457b9a85
jaserud <udjaser@gmail.com>
2020-05-20 12:30:58 -0400

    better type for info

--- src/lectern-client/schema-entities.ts
+++ src/lectern-client/schema-entities.ts
@@ -106,7 +106,7 @@ export interface SchemaValidationError {
   readonly errorType: SchemaValidationErrorTypes;
   readonly index: number;
   readonly fieldName: string;
-  readonly info: any;
+  readonly info: Record<string, any>;
   readonly message: string;
 }
 
