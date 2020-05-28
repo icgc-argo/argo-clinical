c14fcf3964b1222df46962c0f9ea214dea0300e2
ballabadi <bashar.allabadi@oicr.on.ca>
2020-05-28 11:41:33 -0400

    add regex and exmaples to lectern client error msg

--- src/lectern-client/schema-error-messages.ts
+++ src/lectern-client/schema-error-messages.ts
@@ -1,9 +1,9 @@
 const INVALID_VALUE_ERROR_MESSAGE = 'The value is not permissible for this field.';
 const ERROR_MESSAGES: { [key: string]: (errorData: any) => string } = {
   INVALID_FIELD_VALUE_TYPE: () => INVALID_VALUE_ERROR_MESSAGE,
-  INVALID_BY_REGEX: () => INVALID_VALUE_ERROR_MESSAGE,
+  INVALID_BY_REGEX: errData => getRegexErrorMsg(errData.info),
   INVALID_BY_RANGE: () => 'Value is out of permissible range',
-  INVALID_BY_SCRIPT: (error) => error.info.message,
+  INVALID_BY_SCRIPT: error => error.info.message,
   INVALID_ENUM_VALUE: () => INVALID_VALUE_ERROR_MESSAGE,
   MISSING_REQUIRED_FIELD: errorData => `${errorData.fieldName} is a required field.`,
 };
@@ -15,5 +15,13 @@ const schemaErrorMessage = (errorType: string, errorData: any = {}): string => {
     ? ERROR_MESSAGES[errorType](errorData)
     : errorType;
 };
+
+function getRegexErrorMsg(info: any) {
+  let msg = `The value is not a permissible for this field, it must meet the regular expression: "${info.regex}".`;
+  if (info.examples) {
+    msg = msg + ` Examples: ${info.examples}`;
+  }
+  return msg;
+}
 
 export default schemaErrorMessage;
