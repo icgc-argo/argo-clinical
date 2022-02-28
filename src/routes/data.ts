9b795c4212523af384bca8c58b21681980669acf
joneubank <joneubank@gmail.com>
2022-02-28 18:35:39 -0500

    Clinical donor query API updates

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -24,16 +24,25 @@ import clinicalApi from '../clinical/clinical-api';
 
 const router = express.Router();
 
-router.get('/donors', wrapAsync(clinicalApi.findDonors));
-router.delete('/donors', wrapAsync(clinicalApi.deleteDonors));
+// Get and Delete all
+router.get('/donors', wrapAsync(clinicalApi.findDonors)); //  DEPRECATED
+router.delete('/donors', wrapAsync(clinicalApi.deleteDonors)); // TEST ONLY
+
+// Get IDs
 router.get('/donors/id', wrapAsync(clinicalApi.findDonorId));
 router.get('/specimens/id', wrapAsync(clinicalApi.findSpecimenId));
 router.get('/samples/id', wrapAsync(clinicalApi.findSampleId));
 
+// Get Donor Data
+router.get('/program/:programId/donor/:donorId', wrapAsync(clinicalApi.getDonorById));
+router.get('/program/:programId/donors', wrapAsync(clinicalApi.streamProgramDonors));
+
+// Get TSV Data
 router.get(
   '/program/:programId/tsv-export',
   wrapAsync(clinicalApi.getProgramClinicalDataAsTsvsInZip),
-);
+); // DEPRECATED
+router.get('/program/:programId/tsv/all', wrapAsync(clinicalApi.getProgramClinicalDataAsTsvsInZip));
 
 router.patch('/donor/:donorId/completion-stats', wrapAsync(clinicalApi.patchDonorCompletionStats));
 
