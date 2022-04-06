b05006da83d56157fba8647739b75aa93f57ecd8
Dan DeMaria <ddemaria@oicr.on.ca>
2022-04-06 14:32:04 -0400

    Align Routes

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -36,7 +36,7 @@ router.get('/samples/id', wrapAsync(clinicalApi.findSampleId));
 // Get Donor Data
 router.get('/program/:programId/donor/:donorId', wrapAsync(clinicalApi.getDonorById));
 router.get('/program/:programId/donors', wrapAsync(clinicalApi.streamProgramDonors));
-router.get('/program/:programId/submitted-data', wrapAsync(clinicalApi.getProgramClinicalData));
+router.get('/program/:programId/clinical-data', wrapAsync(clinicalApi.getProgramClinicalData));
 
 // Get TSV Data
 router.get(
