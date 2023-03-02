72a953d15807678c9f5cff4b74cadd8e0d5ed05c
Dan DeMaria <ddemaria@oicr.on.ca>
2023-03-02 17:06:49 -0500

    Remove unused import

--- src/clinical/service-worker-thread/tasks.ts
+++ src/clinical/service-worker-thread/tasks.ts
@@ -17,7 +17,7 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import _, { isEmpty, isArray } from 'lodash';
+import _, { isEmpty } from 'lodash';
 import {
   ClinicalEntitySchemaNames,
   aliasEntityNames,
