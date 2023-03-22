a34e3f40effa2a1251c42e4646056cc9a73912c2
Dan DeMaria <ddemaria@oicr.on.ca>
2023-03-22 11:22:11 -0400

    Add Todo

--- src/submission/validation-clinical/therapy.ts
+++ src/submission/validation-clinical/therapy.ts
@@ -48,7 +48,7 @@ export const validate = async (
   let errors: SubmissionValidationError[] = [];
   const treatment = getTreatment(therapyRecord, mergedDonor, errors);
   if (!treatment) return { errors };
-
+  // TODO: Refactor use of push
   checkTreatmentHasCorrectTypeForTherapy(therapyRecord, treatment, errors);
 
   const {
