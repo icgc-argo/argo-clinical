c70c656b6bd4d020622cee68a7bd798121ba8ba9
Dan DeMaria <ddemaria@oicr.on.ca>
2023-03-30 10:27:13 -0400

    Remove map return type

--- src/clinical/service-worker-thread/tasks.ts
+++ src/clinical/service-worker-thread/tasks.ts
@@ -277,9 +277,7 @@ function extractEntityDataFromDonors(
   });
 
   const completionStats: CompletionDisplayRecord[] = donors
-    .map(({ completionStats, donorId, specimens: donorSpecimenRecords }):
-      | CompletionDisplayRecord
-      | undefined => {
+    .map(({ completionStats, donorId, specimens: donorSpecimenRecords }) => {
       // Add display data for Specimen Normal/Tumour completion stats
       const filteredSpecimenData = donorSpecimenRecords.filter(dnaSampleFilter);
       const specimens = { specimens: calculateSpecimenCompletionStats(filteredSpecimenData) };
