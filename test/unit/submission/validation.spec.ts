a131b64b67190602916c6fcf81ecfe66caebcec9
Dan DeMaria <ddemaria@oicr.on.ca>
2023-05-18 20:05:15 -0400

    Testing Fix

--- test/unit/submission/validation.spec.ts
+++ test/unit/submission/validation.spec.ts
@@ -2162,12 +2162,12 @@ describe('data-validator', () => {
           therapyType: ClinicalEntitySchemaNames.RADIATION,
           reference_radiation_treatment_id: 'T_03',
           donorSubmitterId: 'AB2',
-          value: 'T_03',
+          value: 'AB1',
         },
-        message: `The 'reference_radiation_treatment_id' T_03 belongs to submitter_donor_id "DN425", not "DN290"`,
+        message: `The 'reference_radiation_treatment_id' T_03 belongs to submitter_donor_id AB1, not AB2`,
       };
 
-      chai.expect(result.radiation.dataErrors.length).to.eq(2);
+      chai.expect(result.radiation.dataErrors.length).to.eq(1);
       chai.expect(result.radiation.dataErrors).to.deep.include(therapyConflictErr);
     });
 
@@ -2215,7 +2215,7 @@ describe('data-validator', () => {
         message: `The submitter_treatment_id submitted in the "reference_radiation_treatment_id" field is not for radiation treatment.`,
       };
 
-      chai.expect(result.radiation.dataErrors.length).to.eq(3);
+      chai.expect(result.radiation.dataErrors.length).to.eq(2);
       chai.expect(result.radiation.dataErrors).to.deep.include(therapyConflictErr);
     });
 
