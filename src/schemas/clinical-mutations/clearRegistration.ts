f0fd8d490cc8f60073c2781e225634850a0d95ff
Dan DeMaria <ddemaria@oicr.on.ca>
2023-09-11 11:54:43 -0400

    ClearRegistration + Copyright

--- src/schemas/clinical-mutations/clearRegistration.ts
+++ src/schemas/clinical-mutations/clearRegistration.ts
@@ -1,17 +1,34 @@
-// clearClinicalRegistration: async (
-//   obj: unknown,
-//   args: {
-//     shortName: string;
-//     registrationId: string;
-//   },
-//   context: GlobalGqlContext,
-// ) => {
-//   const { Authorization } = context;
-//   const { shortName, registrationId } = args;
-//   const response = await clinicalService.clearRegistrationData(
-//     shortName,
-//     registrationId,
-//     Authorization,
-//   );
-//   return true;
-// },
+/*
+ * Copyright (c) 2023 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { operations } from '../../submission/submission-service';
+
+const clearClinicalRegistration = async (
+  obj: unknown,
+  args: {
+    shortName: string;
+    registrationId: string;
+  },
+) => {
+  const { shortName, registrationId } = args;
+  const response = await operations.deleteRegistration(shortName, registrationId);
+  return true;
+};
+
+export default clearClinicalRegistration;
