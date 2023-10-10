71fa6a1afc892aa574964f7183f8e1965b2691a9
UmmulkiramR <urangwala@oicr.on.ca>
2023-10-10 15:56:54 -0400

    added clinical submission upload rest endpoint

--- src/routes/submission.ts
+++ src/routes/submission.ts
@@ -22,17 +22,14 @@ import multer from 'multer';
 
 import { wrapAsync } from '../middleware';
 import submissionAPI from '../submission/submission-api';
+import uploadClinicalSubmissionsData from '../schemas/clinical-resolvers/uploadClinicalSubmissions';
 
 const router = express.Router({ mergeParams: true });
 
 const upload = multer({ dest: '/tmp' });
 
 router.get('/', wrapAsync(submissionAPI.getActiveSubmissionByProgramId));
-router.post(
-  '/upload',
-  upload.array('clinicalFiles'),
-  wrapAsync(submissionAPI.uploadClinicalTsvFiles),
-);
+router.post('/upload', upload.array('clinicalFiles'), wrapAsync(uploadClinicalSubmissionsData));
 
 router.post('/validate/:versionId', wrapAsync(submissionAPI.validateActiveSubmission));
 router.post('/commit/:versionId', wrapAsync(submissionAPI.commitActiveSubmission));
