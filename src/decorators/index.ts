6e0d42bc98ba413581ec501e931ef88d1383bc34
Dan DeMaria <ddemaria@oicr.on.ca>
2023-10-23 13:12:06 -0400

    Remove Export

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -89,7 +89,7 @@ const verifyEgoApiKey = async (keyString: string): Promise<TokenValidationResult
  * @param providedScopes scopes provided in auth header
  * @returns {boolean} true if provided scopes contains at least one of the requiredScopes
  */
-export const hasScope = (requiredScopes: string[], providedScopes: string[]): boolean =>
+const hasScope = (requiredScopes: string[], providedScopes: string[]): boolean =>
   requiredScopes.some(scope => providedScopes.includes(scope));
 
 const checkAuthorization = async (scopes: string[], request: Request, response: Response) => {
