6076bf57ed5e9220873f0a1dede3211a9334f993
Ciaran Schutte <ciaranschutte@oicr.on.ca>
2024-01-23 09:47:23 -0500

    update util return type

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -17,7 +17,7 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-export const parseBoolString = (queryString: string) => {
+export const parseBoolString = (queryString: string): boolean => {
 	if (queryString) {
 		return queryString.toLowerCase() === 'true';
 	} else {
