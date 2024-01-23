57f490a1164198cb95970cb97cb0c7355f56cb50
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2024-01-23 12:27:18 -0500

    Create update endpoint (#1110)
    * update swagger
    
    * rename routing to nest exceptions
    
    * add missing entity exception api call for POST
    
    * rename old exception api import
    
    * add missing entity exception api
    
    * add permissions check
    
    * wrap async
    
    * update swagger
    
    * remove feature flag
    
    * move parse bool to util
    
    * add create missing entity exception service layer
    
    * add 500 status for not implemented GET
    
    * simplify bool condition
    
    * routing renaming and split into individual files
    
    * update missing entity route params
    
    * update swagger path names
    
    * update swagger
    
    * rename
    
    * swagger updates
    
    * rename index exception route
    
    * update util return type
    
    * add tsdoc
    
    * fix router import
    
    ---------
    
    Co-authored-by: Ciaran Schutte <ciaranschutte@oicr.on.ca>

---
+++ src/exception/missing-entity-exceptions/api.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { HasFullWriteAccess } from '../../decorators';
+import { Request, Response } from 'express';
+import { parseBoolString } from '../../utils/request';
+import * as service from './service';
+
+class MissingEntityExceptionController {
+	@HasFullWriteAccess()
+	async createEntityException(req: Request, res: Response) {
+		const programId = req.params.programId;
+		const donorSubmitterIds = req.body.donorSubmitterIds;
+		const isDryRun = parseBoolString(req.query['dry-run']);
+
+		const result = await service.create({
+			programId,
+			newDonorIds: donorSubmitterIds,
+			isDryRun,
+		});
+
+		if (!result.success) {
+			const { message, errors } = result;
+			return res.status(500).send({ message, errors });
+		} else {
+			return res.status(201).send(result.exception);
+		}
+	}
+}
+
+export default new MissingEntityExceptionController();
