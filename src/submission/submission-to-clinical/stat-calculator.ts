231a58782df93202f189ed2c9b33a45549245201
Jon Eubank <joneubank@gmail.com>
2024-01-28 14:09:08 -0500

    Completion Stats record if they have missing entity exception

--- src/submission/submission-to-clinical/stat-calculator.ts
+++ src/submission/submission-to-clinical/stat-calculator.ts
@@ -149,6 +149,7 @@ export const calculateDonorCoreCompletionStats = (
 		...newCompletionStats,
 		coreCompletionPercentage,
 		coreCompletionDate,
+		hasMissingEntityException,
 	};
 
 	return newCompletionStats;
