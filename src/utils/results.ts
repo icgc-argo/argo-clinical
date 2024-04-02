c76e0a1b3cd848bbd4a80897a6edeb48a15a808a
Dan DeMaria <ddemaria@oicr.on.ca>
2024-04-02 15:09:18 -0400

    Shared AsyncResult, Get Exception function

--- src/utils/results.ts
+++ src/utils/results.ts
@@ -2,6 +2,7 @@
 export type Success<T> = { success: true; data: T };
 export type Failure = { success: false; message: string; errors?: any };
 export type Result<T> = Success<T> | Failure;
+export type AsyncResult<T> = Promise<Result<T>>;
 
 // helpers
 export const success = <T>(data: T): Success<T> => ({ success: true, data });
